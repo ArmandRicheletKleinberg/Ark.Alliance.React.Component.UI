c73f247b8890655c20ea42e5b1f1f38426ecb9f5
ArmandRIchelet <arkleinberg@gmail.com>
2025-12-28 19:45:25 -0400

    Revert "Minor bug fix for export Css and lazy-loaded in Chart3D.tsx"
    This reverts commit 46991cd6ba056cc0393767f40a16b58b53039a30.

--- Ark.Alliance.React.Component.UI/src/components/Chart3D/Scene/index.ts
+++ Ark.Alliance.React.Component.UI/src/components/Chart3D/Scene/index.ts
@@ -1,12 +1,9 @@
 /**
  * @fileoverview Scene Barrel Export
  * @module components/Chart3D/Scene
- * 
- * Note: Scene3D is not exported here as it is lazy-loaded in Chart3D.tsx
  */
 
 export { AxisSystem } from './AxisSystem';
 export { ChartShapes } from './ChartShapes';
 export { SurfaceRibbon } from './SurfaceRibbon';
-// Scene3D is lazy-loaded directly in Chart3D.tsx - do not export here
-
+export { Scene3D } from './Scene3D';
--- Ark.Alliance.React.Component.UI/src/components/Chart3D/index.ts
+++ Ark.Alliance.React.Component.UI/src/components/Chart3D/index.ts
@@ -8,6 +8,4 @@ export * from './Chart3D.viewmodel';
 export * from './Chart3D.constants';
 export { Chart3D, type Chart3DProps } from './Chart3D';
 export { ControlPanel } from './ControlPanel';
-export { AxisSystem, ChartShapes, SurfaceRibbon } from './Scene';
-// Note: Scene3D is lazy-loaded internally and not exported from public API
-
+export { Scene3D, AxisSystem, ChartShapes, SurfaceRibbon } from './Scene';
