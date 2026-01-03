7ca2b067f589ee1dee9abc0d215856f2dc1e5109
ArmandRIchelet <arkleinberg@gmail.com>
2026-01-03 14:54:20 -0400

    chore(release): v1.1.4
    Bump version to 1.1.4 and update badges

--- Ark.Alliance.React.Component.UI/vite.config.ts
+++ Ark.Alliance.React.Component.UI/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       include: ['src'],
       outDir: 'dist',
       insertTypesEntry: true,
-      tsconfigPath: './tsconfig.json',
+      tsconfigPath: './tsconfig.app.json',
       copyDtsFiles: true,
     }),
   ],
